35c730402ed267d36859400c8ec0dd37baddfce6
Kamini Singh <www.kriya1@gmail.com>
2024-04-11 14:36:15 +0530

    Email Validation Added

--- src/main/java/com/projects/cardpayment/utils/Validation.java
+++ src/main/java/com/projects/cardpayment/utils/Validation.java
@@ -13,38 +13,40 @@ import com.projects.cardpayment.entities.Card;
 @Component
 public class Validation {
 
-    Logger logger = LoggerFactory.getLogger(Validation.class);
+	Logger logger = LoggerFactory.getLogger(Validation.class);
 
-    public boolean createCardRequestValidation(Card card) {
-        boolean validationPassed = true;
+	public boolean createCardRequestValidation(Card card) {
+		boolean validationPassed = true;
 
-        if (Objects.isNull(card.getCardHolderFirstName()) || card.getCardHolderFirstName().trim().length() < 3) {
-            logger.info("Card Holder FirstName Validation Result :: FAILED");
-            validationPassed = false;
-        }
+		if (Objects.isNull(card.getCardHolderFirstName()) || card.getCardHolderFirstName().trim().length() < 3) {
+			logger.info("Card Holder FirstName Validation Result :: FAILED");
+			validationPassed = false;
+		}
 
-        if (Objects.isNull(card.getCardHolderLastName()) || card.getCardHolderLastName().trim().length() < 3) {
-            logger.info("Card Holder LastName Validation Result :: FAILED");
-            validationPassed = false;
-        }
-        
-              String email = card.getEmail();
-        String regex = "[^@]{5,20}[@][A-z]+[.][a-z]{3}";
+		if (Objects.isNull(card.getCardHolderLastName()) || card.getCardHolderLastName().trim().length() < 3) {
+			logger.info("Card Holder LastName Validation Result :: FAILED");
+			validationPassed = false;
+		}
 
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(email);
+		if (Objects.isNull(card.getEmail())) {
+			logger.info("Card Email Validation isNULL :: FAILED");
+			validationPassed = false;
 
-        if (matcher.matches()) {
-        	logger.info("Card Email Validation Result :: SUCCESS");
-        	validationPassed=true;
-        	
-        } else {
-        	logger.info("CarD Email Validation Result :: FAILED");
-        	validationPassed=false;
-        	
-        }
-    
+		} else {
+			String email = card.getEmail();
+			String regex = "[^@]{5,20}[@][A-z]+[.][a-z]{3}";
 
-        return validationPassed;
-    }
+			Pattern pattern = Pattern.compile(regex);
+			Matcher matcher = pattern.matcher(email);
+
+			if (matcher.matches()) {
+				logger.info("Card Email Validation Result :: SUCCESS");
+				validationPassed = true;
+			} else {
+				logger.info(" Email Validation :: FAILED");
+				validationPassed = false;
+			}
+		}
+		return validationPassed;
+	}
 }

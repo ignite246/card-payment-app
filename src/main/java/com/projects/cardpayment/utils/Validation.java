c65928c260625fc11dca53dc0cb9e42f4abec8be
Kamini Singh <www.kriya1@gmail.com>
2024-04-15 19:25:51 +0530

    Added regex and validation for firstName,lastName,bankName.

--- src/main/java/com/projects/cardpayment/utils/Validation.java
+++ src/main/java/com/projects/cardpayment/utils/Validation.java
@@ -18,17 +18,17 @@ public class Validation {
 	public boolean createCardRequestValidation(Card card) {
 		boolean validationPassed = true;
 
-		if (Objects.isNull(card.getCardHolderFirstName()) || card.getCardHolderFirstName().trim().length() < 3) {
+		if (!isFirstnameLastnameBanknamePatternValid(card.getCardHolderFirstName())) {
 			logger.info("Card Holder FirstName Validation Result :: FAILED");
 			validationPassed = false;
 		}
 
-		if (Objects.isNull(card.getCardHolderLastName()) || card.getCardHolderLastName().trim().length() < 3) {
+		if (!isFirstnameLastnameBanknamePatternValid(card.getCardHolderLastName())) {
 			logger.info("Card Holder LastName Validation Result :: FAILED");
 			validationPassed = false;
 		}
 
-		if (Objects.isNull(card.getCardBankName()) || card.getCardBankName().trim().length() < 3) {
+		if (!isFirstnameLastnameBanknamePatternValid(card.getCardBankName())) {
 			logger.info("Card bankName Validation Result :: FAILED {}", card.getCardBankName());
 			validationPassed = false;
 		}
@@ -105,6 +105,18 @@ public class Validation {
 		Matcher matcher = pattern.matcher(expiryDate);
 
 		return matcher.matches();
+	}
+	
+	public boolean isFirstnameLastnameBanknamePatternValid(String name ) {
+		logger.info("Firstname lastname bankname To Be Validated :: {}",name);	
+		String regex = "[A-z]{3,20}";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(name);
+		
+		
+		return matcher.matches();
+		
+		
 	}
 
 }

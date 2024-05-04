a85890cd4e8405fe11d27ff26785e029c6c5cc18
Rahul Kumar <rk7870382701@gmail.com>
2024-05-04 22:58:53 +0530

    space removed and cross origin is allowed in Card-API

--- src/main/java/com/projects/cardpayment/controllers/CardController.java
+++ src/main/java/com/projects/cardpayment/controllers/CardController.java
@@ -12,15 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.itextpdf.text.DocumentException;
 import com.projects.cardpayment.entities.Card;
@@ -31,6 +23,7 @@ import com.projects.cardpayment.response.dto.GetCardBalanceInBetweenResDTO;
 import com.projects.cardpayment.service.CardService;
 import com.projects.cardpayment.utils.Validation;
 
+@CrossOrigin(value = "*")
 @RestController
 @RequestMapping("/card-api")
 public class CardController {
